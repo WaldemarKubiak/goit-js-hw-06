7a70bb0696200d199e0a2f48e678814111707b7f
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-02-18 10:47:49 +0100

    Task-02 finished and ready to check

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsElements = document.querySelector("#ingredients");
+
+const listGreens = ingredients.map((ingredient) => {
+  const listGreens = document.createElement("li");
+  listGreens.textContent = ingredient;
+  listGreens.classList.add("item");
+  return listGreens;
+});
+
+ingredientsElements.append(...listGreens);

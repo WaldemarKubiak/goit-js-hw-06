cff48fe7785c360f79b0b0738fa5366d74c8748d
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-02-19 13:32:37 +0100

    Task-09 finished and ready to check

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const changeColBtn = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+const changeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+};
+
+changeColBtn.addEventListener("click", changeColor);
+
+// console.log(body);
+// console.log(changeColBtn);
+// console.log(getRandomHexColor());

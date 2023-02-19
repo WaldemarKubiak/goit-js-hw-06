7400e5e962e6386153665d41f0c73ddfd37b7fa0
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-02-19 12:52:49 +0100

    Task-08 finished and ready to check

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formLogin = document.querySelector(".login-form");
 
+formLogin.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Proszę, uzupełnij puste pola.");
+  }
+
+  const userLoginDetails = { Email: email.value, Password: password.value };
+
+  console.log(userLoginDetails);
+  event.currentTarget.reset();
+}
+
+// console.log(formLogin);
